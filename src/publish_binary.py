bfe2c91470fca73c99f98d4572e2e2dea13cbc53
Michael Koval <mkoval@cs.cmu.edu>
2015-10-09 13:56:28 -0400

    Added binarization code.

--- src/publish_binary.py
+++ src/publish_binary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import numpy
 import rospy
+import time
 
 
 class TakktileSubscriber(object):
@@ -27,9 +28,29 @@ class TakktileSubscriber(object):
         self.tare_srv = rospy.Service(
             self.tare_topic, Trigger, self.tare_callback)
 
+        return self
+
     def __exit__(self, type, value, traceback):
         self.takktile_sub.unregister()
         self.tare_srv.shutdown()
+
+    def tare(self):
+        rospy.loginfo('Starting tare.')
+        time_before = time.time()
+
+        with self.lock:
+            # Wait for the queue to fill up.
+            while len(self.queue) < self.queue.maxlen:
+                self.condition.wait()
+
+            # Compute the tare offset.
+            pressures = numpy.array(
+                [msg.pressure for msg in self.queue], dtype='float')
+            self.tare_offset = numpy.mean(pressures, axis=0)
+
+        time_after = time.time()
+        rospy.loginfo('Completed tare after %.3f seconds.',
+            time_after - time_before)
 
     def data_callback(self, takktile_msg):
         with self.lock:
@@ -50,20 +71,37 @@ class TakktileSubscriber(object):
     def tare_callback(self, request):
         from std_srvs.srv import TriggerResponse
 
-        with self.lock:
-            # Wait for the queue to fill up.
-            while len(self.queue) < self.queue.maxlen:
-                self.condition.wait()
-
-            # Compute the tare offset.
-            pressures = numpy.array(
-                [msg.pressure for msg in self.queue], dtype='float')
-            self.tare_offset = numpy.mean(pressures, axis=0)
+        self.tare()
 
         return TriggerResponse(success=True, message='Tare complete.')
 
+
+class TakktileBinarizer(object):
+    def __init__(self, groups, callback, threshold=4.):
+        self.group_map = list(groups)
+        self.threshold = float(threshold)
+        self.all_groups = set(groups)
+        self.callback = callback
+
+    def binarize(self, takktile_msg, pressure_tared):
+        if len(self.group_map) != len(pressure_tared):
+            rospy.logwarn(
+                'Received unexpected number of pressure values: expected %d,'
+                ' got %d.', len(self.group_map), len(pressure_tared))
+            return
+
+        pressure_mask = (pressure_tared > self.threshold)
+        active_groups = set(
+            group for group, is_active in zip(self.group_map, pressure_mask)
+            if is_active)
+        print active_groups
+        output = { group: group in active_groups for group in self.all_groups }
+
+        return self.callback(takktile_msg, output)
+
 def callback(takktile_msg, pressure):
-    print pressure
+    #print pressure
+    pass
 
 def main():
     import argparse
@@ -89,7 +127,12 @@ def main():
         
     marker_pub = rospy.Publisher('takktile/markers', MarkerArray, queue_size=1)
 
-    with TakktileSubscriber('takktile/raw', 'takktile/tare', callback):
+    thresholder = TakktileBinarizer(groups_data, callback)
+    sensor_sub = TakktileSubscriber(
+        'takktile/raw', 'takktile/tare', thresholder.binarize)
+
+    with sensor_sub:
+        sensor_sub.tare()
         rospy.spin()
 
     rospy.spin()
